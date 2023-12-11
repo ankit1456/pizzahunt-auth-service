ea5c14d023fe6f9c30484a0b49b58b80f3466b89
ankit tripathi <ankittripathi1456@gmail.com>
2023-12-11 18:32:56 +0530

    setup express app

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,19 @@
-function Login(username: string): string {
-  return username;
-}
+import app from './app';
+import { Config } from './config';
 
-Login('ankit');
+const startServer = () => {
+  const { PORT } = Config;
+
+  try {
+    app.listen(PORT, () =>
+      // eslint-disable-next-line no-console
+      console.log(`Server running on url http://localhost:${PORT}`)
+    );
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+startServer();
