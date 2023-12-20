055d89826dc6511f475fd5623e4dcf96679bfe65
ankit tripathi <ankittripathi1456@gmail.com>
2023-12-21 01:24:20 +0530

    add test for user persistence

--- src/tests/users/register.spec.ts
+++ src/tests/users/register.spec.ts
@@ -1,7 +1,26 @@
 import request from 'supertest';
 import app from '../../app';
+import { DataSource } from 'typeorm';
+import { AppDataSource } from '../../config/data-source';
+import { truncateTables } from '../utils';
+import { User } from '../../entity/User';
 
 describe('POST /api/auth/register', () => {
+  let connection: DataSource;
+
+  beforeAll(async () => {
+    connection = await AppDataSource.initialize();
+  });
+
+  beforeEach(async () => {
+    // Database truncate
+    await truncateTables(connection);
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
+
   describe('success cases', () => {
     it('should return 201 statusCode', async () => {
       // AAA
@@ -32,15 +51,18 @@ describe('POST /api/auth/register', () => {
     });
 
     it('should persist the user in the database', async () => {
-      // const userData = {
-      //   firstName: 'Ankit',
-      //   lastName: 'Tripahi',
-      //   email: 'ankit@gmail.com',
-      //   password: 'test1234'
-      // };
-      // const response = await request(app)
-      //   .post('/api/auth/register')
-      //   .send(userData);
+      const userData = {
+        firstName: 'Ankit',
+        lastName: 'Tripahi',
+        email: 'ankit@gmail.com',
+        password: 'test1234'
+      };
+      await request(app).post('/api/auth/register').send(userData);
+
+      const userRespository = connection.getRepository(User);
+      const users = await userRespository.find();
+
+      expect(users).toHaveLength(1);
     });
   });
   describe('failure cases', () => {});
