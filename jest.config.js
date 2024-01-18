f76fc2c3d4eb6bbef308c6033610e1b1287dc6e5
ankit tripathi <ankittripathi1456@gmail.com>
2024-01-18 16:00:34 +0530

    set test timeout to 6000

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   verbose: true,
-  testTimeout: 7000,
+  testTimeout: 6000,
   collectCoverage: true,
   coverageProvider: 'v8',
   collectCoverageFrom: ['src/**/*.ts', '!tests/**', '!**/node_modules/**']
