6710bd274b93a235a0a3d6bd8a2952f8ef616f07
ankit tripathi <ankittripathi1456@gmail.com>
2024-01-18 15:42:17 +0530

    increse test timeout

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   verbose: true,
+  testTimeout: 7000,
   collectCoverage: true,
   coverageProvider: 'v8',
   collectCoverageFrom: ['src/**/*.ts', '!tests/**', '!**/node_modules/**']
