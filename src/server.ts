6c56ecf08b3f948a30b814b00ccada311d6dd0f1
ankit tripathi <ankittripathi1456@gmail.com>
2024-01-25 17:34:58 +0530

    add uncaughtException, unhandledRejection and SIGTERM handlers

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,22 @@ import logger from './config/logger';
 import app from './app';
 import { Config } from './config';
 import { AppDataSource } from './config/data-source';
+import { Server } from 'http';
 
+process.on('uncaughtException', (err) => {
+  logger.info('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  logger.error(err.name, { message: err.message });
+  process.exit(1);
+});
+
+let server: Server;
 const startServer = async () => {
   const { PORT } = Config;
 
   try {
     await AppDataSource.initialize();
     logger.info('Database connected successfully 😊');
-    app.listen(PORT, () =>
+    server = app.listen(PORT, () =>
       logger.info(`Server running on port ${PORT}`, {
         success: 'Server Started Successfully 😊😊'
       })
@@ -25,3 +33,18 @@ const startServer = async () => {
 };
 
 void startServer();
+
+process.on('unhandledRejection', (err: Error) => {
+  logger.error(err.name, { message: err.message });
+  logger.info('UnhandledRejection , shutting down 😶');
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM RECIEVED ,Shutting down gracefully 👋');
+  server.close(() => {
+    logger.error('💥 Process Terminated');
+  });
+});
