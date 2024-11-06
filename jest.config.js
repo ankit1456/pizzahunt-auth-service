be0665692a363f599985ece38b587c18b83aa840
ankit tripathi <ankittripathi1456@gmail.com>
2024-11-06 13:58:26 +0530

    add path alias for tests

--- jest.config.js
+++ jest.config.js
@@ -12,5 +12,17 @@ module.exports = {
     '!tests/**',
     '!**/node_modules/**',
     '!src/migration/**'
-  ]
+  ],
+  moduleNameMapper: {
+    '^@config(.*)$': '<rootDir>/src/config$1',
+    '^@middlewares(.*)$': '<rootDir>/src/middlewares$1',
+    '^@routes(.*)$': '<rootDir>/src/routes$1',
+    '^@utils(.*)$': '<rootDir>/src/utils$1',
+    '^@controllers(.*)$': '<rootDir>/src/controllers$1',
+    '^@services(.*)$': '<rootDir>/src/services$1',
+    '^@validators(.*)$': '<rootDir>/src/validators$1',
+    '^@entity(.*)$': '<rootDir>/src/entity$1',
+    '^@customTypes(.*)$': '<rootDir>/src/customTypes$1',
+    '^@src/(.*)$': '<rootDir>/src/$1'
+  }
 };
